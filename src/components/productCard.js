aaa90dd64d649b7593c4e7b79b794bc99c7f9141
Joel <joel.weber@live.nl>
2019-03-13 21:17:58 +0100

    oepsie

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -7,78 +7,79 @@ import Button from '@material-ui/core/Button'
 import { AddToCart } from 'react-snipcart'
 import { Link } from 'gatsby'
 
-const ProductCard = ({ classes, product }) => {}
-;<div
-  style={{
-    flexGrow: 1,
-  }}
->
-  <Paper
+const ProductCard = ({ classes, product }) => (
+  <div
     style={{
-      padding: 15,
-      margin: 'auto',
+      flexGrow: 1,
     }}
   >
-    <Grid container justify="center" spacing={16}>
-      <Grid item>
-        <ButtonBase
-          style={{
-            width: 128,
-            height: 128,
-          }}
-        >
-          <Link to={product.url}>
-            <img
-              style={{
-                margin: 'auto',
-                display: 'block',
-                maxWidth: '100%',
-                maxHeight: '100%',
-              }}
-              alt="complex"
-              src={product.image}
-            />
-          </Link>
-        </ButtonBase>
-      </Grid>
-      <Grid item xs={12} sm container>
-        <Grid item xs container direction="column" spacing={16}>
-          <Grid item xs>
-            <Link
-              to={product.url}
-              style={{
-                color: 'black',
-                textDecoration: 'none',
-              }}
-            >
-              <Typography gutterBottom variant="subtitle1">
-                {product.name}
-              </Typography>
+    <Paper
+      style={{
+        padding: 15,
+        margin: 'auto',
+      }}
+    >
+      <Grid container justify="center" spacing={16}>
+        <Grid item>
+          <ButtonBase
+            style={{
+              width: 128,
+              height: 128,
+            }}
+          >
+            <Link to={product.url}>
+              <img
+                style={{
+                  margin: 'auto',
+                  display: 'block',
+                  maxWidth: '100%',
+                  maxHeight: '100%',
+                }}
+                alt="complex"
+                src={product.image}
+              />
             </Link>
-          </Grid>
-          <Grid item>
-            <Button variant="outlined" color="primary" size="small">
-              <AddToCart
-                data={{
-                  id: product.id,
-                  name: product.name,
-                  url: '/',
-                  price: product.price,
-                  image: product.image,
-                  description: product.description,
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={16}>
+            <Grid item xs>
+              <Link
+                to={product.url}
+                style={{
+                  color: 'black',
+                  textDecoration: 'none',
                 }}
               >
-                In winkelwagen
-              </AddToCart>
-            </Button>
+                <Typography gutterBottom variant="subtitle1">
+                  {product.name}
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary" size="small">
+                <AddToCart
+                  data={{
+                    id: product.id,
+                    name: product.name,
+                    url: '/',
+                    price: product.price,
+                    image: product.image,
+                    description: product.description,
+                  }}
+                >
+                  In winkelwagen
+                </AddToCart>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1">€{product.price}</Typography>
           </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="subtitle1">€{product.price}</Typography>
-        </Grid>
       </Grid>
-    </Grid>
-  </Paper>
-</div>
+    </Paper>
+  </div>
+)
 
 export default ProductCard
